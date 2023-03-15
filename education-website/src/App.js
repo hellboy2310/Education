b4e42a0e68184befc40643f736c4355da96438aa
hellboy2310 <bhaveshpurohit2310@gmail.com>
2023-03-16 00:40:11 +0530

    working on sidebar

--- education-website/src/App.js
+++ education-website/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import SideBar from './components/sideBar';
 
 function App() {
   return (
    <div className='App_Container'>
-    <NavBar></NavBar>
+    
+    
+    
+    <NavBar></NavBar>  
+    <SideBar></SideBar>
+    
+    
     
    </div>
   );
---
+++ education-website/src/components/sideBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './sideBar.css';
+function SideBar() {
+  return (
+    <div className='sidebar_maincontainer'>
+        <div className='logo_container'>
+            <h1>Miles Education</h1>                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        </div>
+        <div className='button_container'>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Launchpad</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">CMA Part-1</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">CMA Part-2</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Study Planner</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Webinar</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Certifications</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Accounting.101</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Forum</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Offline Class</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Study Launge</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Mentoring</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Practice Exam</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">MAC</button>
+        <button type="button" class="btn btn-outline-secondary sidebar_button">Assesments</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default SideBar
