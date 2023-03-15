9ae165f0945c0017347e6c0d3b7cf23ea7721cf7
hellboy2310 <bhaveshpurohit2310@gmail.com>
2023-03-15 23:12:14 +0530

    Navbar static done

---
+++ education-website/src/NavBar.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './NavBar.css'
+function NavBar() {
+  return (
+    <div  className='NavBar_Container'>
+        <ul className="nav justify-content-end">
+  <li className="nav-item">
+  
+  <a className="nav-link disabled" href="#"><i class="fa fa-suitcase" aria-hidden="true"></i></a>
+  </li>
+  <li className="nav-item">
+  <a className="nav-link disabled" href="#"><i class="fa fa-calculator" aria-hidden="true"></i></a>
+  
+    
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled" href="#"><i class="fa fa-video-camera" aria-hidden="true"></i></a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled"><i class="fa fa-bell" aria-hidden="true"></i></a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled" href="#"><i class="fa fa-book" aria-hidden="true"></i></a>
+    
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled" href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+    
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link disabled" href="#"><i class="fa fa-address-book-o" aria-hidden="true"></i></a>
+    
+  </li>
+
+
+  <li className="nav-item">
+    <a className="nav-link disabled" href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>
+    
+  </li>
+
+  <li className="nav-item">
+    <a className="nav-link disabled" href="#"><i class="fa fa-user" aria-hidden="true"></i></a>
+    
+  </li>
+  
+  
+
+  
+  
+</ul>
+    </div>
+  )
+}
+
+export default NavBar
