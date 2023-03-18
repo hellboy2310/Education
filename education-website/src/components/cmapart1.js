14b86a77a5289c018e2be553d45c132799f97fbe
hellboy2310 <bhaveshpurohit2310@gmail.com>
2023-03-18 16:22:37 +0530

    cmapart1 half website done

--- education-website/src/components/cmapart1.js
+++ education-website/src/components/cmapart1.js
@@ -2,30 +2,51 @@ import React from 'react'
 import './cmapart1.css'
 
 function Cmapart1() {
-  return (
-    
-  <div class="row Cmapart1_main_container" >
-  <div class="col-sm-12 col-lg-6 cmacard1">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-  <div class="col-sm-12 col-lg-6 cmacard2">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
-  </div>
-</div>
-  
-  )
+    return (
+        
+
+            <div class="row Cmapart1_main_container" >
+                <div class=" col-lg-5 ">
+                    <div class="card cmacard1 col-sm-12">
+                        <div class="card-body">
+                            <h2 class="card-title Cma_heading">Cma final planning performance and analytics</h2>
+                            <div class="card card_container ">
+                                <div class="card-body  box-holding-buttons ">
+                                    <div class="btn-toolbar content_buttons" role="toolbar" aria-label="Toolbar with button groups">
+                                        <div class="btn-group mr-2 " role="group" aria-label="First group">
+                                            <button type="button" class="btn btn-secondary i_am_button"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>
+
+                                        </div>
+                                        <div class="btn-group mr-2" role="group" aria-label="Second group">
+                                            <button type="button" class="btn btn-secondary i_am_button_holding-subjects"><i class="fa fa-home" aria-hidden="true"></i></button>
+                                            <button type="button" class="btn btn-secondary i_am_button_holding-subjects">A</button>
+                                            <button type="button" class="btn btn-secondary i_am_button_holding-subjects">B</button>
+                                            <button type="button" class="btn btn-secondary i_am_button_holding-subjects">C</button>
+                                            <button type="button" class="btn btn-secondary i_am_button_holding-subjects">D</button>
+                                            <button type="button" class="btn btn-secondary i_am_button_holding-subjects">E</button>
+                                            <button type="button" class="btn btn-secondary i_am_button_holding-subjects">F</button>
+                                        </div>
+                                        <div class="btn-group" role="group" aria-label="Third group">
+                                            <button type="button" class="btn btn-secondary i_am_button"><i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-lg-7 ">
+                    <div class="card cmacard2">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+    )
 }
 
 export default Cmapart1
