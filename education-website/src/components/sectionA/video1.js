edc922d5f5c702532f4a395fe3907002677fb26d
hellboy2310 <bhaveshpurohit2310@gmail.com>
2023-03-19 21:31:28 +0530

    video feature unsuccessful

--- education-website/src/components/sectionA/video1.js
+++ education-website/src/components/sectionA/video1.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { videos } from '../videoData';
+import {Container} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Video1() {
-    let videosArr = videos.results[0];
+    
     return (
-        <>
-      <video src='./sectionA_video1.mp4'>
-
-      </video>
-      
-    </>
+    
+   <div class='pid'>
+    <Container>
+    <iframe src = 'https://www.youtube.com/watch?v=2F8zBNzmROM&t=6s&ab_channel=BhaveshPurohit' allowFullScreen title = 'youtube video'></iframe>
+      </Container>
+    
+    </div> 
+    
     );
 };
 
